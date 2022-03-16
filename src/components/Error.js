dd991bb993926bb07f202dae86fb0946367b3598
Ryan <ryan@thecpafirm.com>
2022-03-16 13:13:31 -0600

    initial setupa and creation

---
+++ src/components/Error.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+let Error = () => {
+
+    return (
+        <main>
+            <div>
+                <div className='aboutDiv'>
+                    <h1>Whoops! Error: 404</h1>
+                    <p>The page you seem to be looking for doesn't appear to be here.</p>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Error
